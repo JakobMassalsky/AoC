790b92dddaab80a6e5424548603f827318ff5876
jakob maßalsky <jakob.massalsky@gmail.com>
2022-12-01 11:59:57 +0100

    stuff

--- AoC_22/AoC_22/src/main.rs
+++ AoC_22/AoC_22/src/main.rs
@@ -3,29 +3,32 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    if let Ok(lines) = read_lines("./input/input_01") {
-
-        let mut sums: Vec<i32> = Vec::new();
-        let mut sum = 0;
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(number) = line {
-                if number.is_empty() {
-                    sums.push(sum);
-                    sum = 0;
-                } else {
-                    sum += number.parse::<i32>().unwrap();
-                }
-            }
+    let lines = read_lines("./input/input_01");
+    let mut sums: Vec<i32> = Vec::new();
+    let mut sum = 0;
+    // Consumes the iterator, returns an (Optional) String
+    for line in lines {
+        if line.is_empty() {
+            sums.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<i32>().unwrap();
         }
-        sums.sort_by(|a, b| b.cmp(a));
-        let cal: i32 = sums[0..3].iter().sum();
-        println!("{}", cal);
     }
+    sums.sort_by(|a, b| b.cmp(a));
+    let cal: i32 = sums[0..3].iter().sum();
+    println!("{}", cal);
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn read_lines<P>(filename: P) -> Vec<String>
+where P: AsRef<Path> {
+    let mut l: Vec<String> = Vec::new();
+    if let Ok(file) = File::open(filename) {
+        for line in io::BufReader::new(file).lines() {
+            if let Ok(s) = line {
+                l.push(s);
+            }
+        }
+    }
+    return l;
 }
