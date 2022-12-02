3a37e6ab1ff48684256fcc5a6d7c611a1926519f
jmassals <jmassals@hs-mittweida.de>
2022-12-02 16:25:11 +0100

    bruh2

--- AoC_22/AoC_22/src/days/day_02.rs
+++ AoC_22/AoC_22/src/days/day_02.rs
@@ -6,19 +6,19 @@ pub fn solve() -> SolutionPair {
     let mut  s1 = 0;
     let mut  s2 = 0;
     for (e, m) in lines.iter().map(|x| x.split_once(" ").unwrap()) {
-        s1 += match m { "X" => 1, "Y" => 2, _ => 3};
-        s2 += match (m, e) {
-            ("X", "B") | ("Y", "C") | ("Z", "A") => 0,
-            ("X", "A") | ("Y", "B") | ("Z", "C") => 3,
-            _ => 6,
-        };
+        s1 += match m { "X" => 1, "Y" => 2, _ => 3}
+            + match (m, e) {
+                ("X", "B") | ("Y", "C") | ("Z", "A") => 0,
+                ("X", "A") | ("Y", "B") | ("Z", "C") => 3,
+                _ => 6,
+            };
 
-        s2 += match m { "X" => 0, "Y" => 3, _ => 6};
-        s2 += match (m, e) {
-            ("X", "B") | ("Y", "A") | ("Z", "C") => 1,
-            ("X", "C") | ("Y", "B") | ("Z", "A") => 2,
-            _ => 3,
-        };
+        s2 += match m { "X" => 0, "Y" => 3, _ => 6}
+            + match (m, e) {
+                ("X", "B") | ("Y", "A") | ("Z", "C") => 1,
+                ("X", "C") | ("Y", "B") | ("Z", "A") => 2,
+                _ => 3,
+            };
     }
 
     (Solution::U64(s1), Solution::U64(s2))
