63e648e3cf2ee86fdbbb9d69d883d7d873896a8c
jmassals <jmassals@hs-mittweida.de>
2022-12-06 12:47:02 +0100

    cleaner

--- AoC_22/AoC_22/src/days/day_06.rs
+++ AoC_22/AoC_22/src/days/day_06.rs
@@ -4,27 +4,22 @@ use crate::{Solution, SolutionPair, etc::utils};
 
 ///////////////////////////////////////////////////////////////////////////////
 
+fn get_sol(chars: &Vec<char>, n: usize) -> u64 {
+    for (i, c) in chars.windows(n).enumerate() {
+        if c.iter().all_unique() {
+            return (i+n) as u64;
+        }
+    }
+    return 0;
+}
+
 pub fn solve() -> SolutionPair {
 
     let lines = utils::read_lines("./input/input_06");
+    let chars = lines[0].chars().collect::<Vec<char>>();
 
-    let mut rb = ['0', '0', '0', '0'];
-    let mut rb2 = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'];
-    let mut sol1: u64 = 0;
-    let mut sol2: u64 = 0;
-
-    for (i, c) in lines[0].char_indices() {
-        rb[i%4] = c;
-        rb2[i%14] = c;
-        if rb.iter().unique().count() == 4 && i >= 4 && sol1 == 0 { 
-            sol1 = i as u64 + 1;
-        }
-        if rb2.iter().unique().count() == 14 && i >= 14 && sol2 == 0 {
-            sol2 = i as u64 + 1;
-            break;
-        }
-    }
-
+    let sol1 = get_sol(&chars, 4);
+    let sol2 = get_sol(&chars, 14);
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
