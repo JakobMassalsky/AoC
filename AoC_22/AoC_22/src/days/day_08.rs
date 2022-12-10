035fe9d533b51ba8c47fb570c10d4a9c5480bc7b
jmassals <jmassals@hs-mittweida.de>
2022-12-10 17:41:12 +0100

    day 8 dynamic 3

--- AoC_22/AoC_22/src/days/day_08.rs
+++ AoC_22/AoC_22/src/days/day_08.rs
@@ -7,33 +7,54 @@ use ndarray::{Array2, Axis, s};
 ///////////////////////////////////////////////////////////////////////////////
 
 fn get_los(filler: &mut ndarray::ArrayBase<ndarray::OwnedRepr<usize>, ndarray::Dim<[usize; 2]>>, 
-    iter: &mut dyn Iterator<Item = (usize, ndarray::ArrayBase<ndarray::ViewRepr<&i32>, ndarray::Dim<[usize; 1]>>)>,
-    rev: bool) {
-    let mut sizes_last_seen = [[0; 10]; 99];
-    if rev {
-        for (j, row) in iter {
-            let mut size_last_seen = [0; 10];
-            for (i, h) in row.iter().enumerate() {
-                *filler.get_mut([j, i]).unwrap() 
-                    *= (i - size_last_seen[(*h as usize)..].iter().max().unwrap())
-                    * (j - sizes_last_seen[i][(*h as usize)..].iter().max().unwrap());
-                size_last_seen[*h as usize] = i;
-                sizes_last_seen[i][*h as usize] = j;
+    a: &ndarray::ArrayBase<ndarray::OwnedRepr<i32>, ndarray::Dim<[usize; 2]>>,) {
+    const SIZE: usize = 99;
+    let mut sizes_last_seen = [[0; 10]; SIZE];
+    let mut last_loss = [[false; 10]; SIZE];
+    let mut size_last_seen;
+    let mut last_los;
+    for x in 0..SIZE {
+        size_last_seen = [0; 10];
+        last_los = [false; 10];
+        for y in 0..SIZE {
+            let h = a[[x, y]];
+            *filler.get_mut([x, y]).unwrap() 
+                *= (y - size_last_seen[(h as usize)..].iter().max().unwrap())
+                * (x - sizes_last_seen[y][(h as usize)..].iter().max().unwrap());
+            for (lh, l) in last_los[..=(h as usize)].iter_mut().enumerate() {
+                if *l {
+                    *filler.get_mut([x, size_last_seen[lh]]).unwrap()
+                        *= y - size_last_seen[lh];
+                    *l = false;
+                }
             }
+            for (lh, l) in last_loss[y][..=(h as usize)].iter_mut().enumerate() {
+                if *l {
+                    *filler.get_mut([sizes_last_seen[y][lh], y]).unwrap()
+                        *= x - sizes_last_seen[y][lh];
+                    *l = false;
+                }
+            }
+            size_last_seen[h as usize] = y;
+            sizes_last_seen[y][h as usize] = x;
+            last_los[h as usize] = true;
+            last_loss[y][h as usize] = true;
         }
-    } else {
-        for (j, row) in iter {
-            let mut size_last_seen = [0; 10];
-            for (i, h) in row.iter().rev().enumerate() {
-                *filler.get_mut([98 - j, 98 - i]).unwrap() 
-                    *= (i - size_last_seen[(*h as usize)..].iter().max().unwrap())
-                    * (j - sizes_last_seen[i][(*h as usize)..].iter().max().unwrap());
-                size_last_seen[*h as usize] = i;
-                sizes_last_seen[i][*h as usize] = j;
+        for lh in 0..=9 {
+            if last_los[lh] {
+                *filler.get_mut([x, size_last_seen[lh]]).unwrap()
+                    *= SIZE - 1 - size_last_seen[lh];
             }
         }
     }
-
+    for y in 0..SIZE {
+        for lh in 0..=9 {
+            if last_loss[y][lh] {
+                *filler.get_mut([sizes_last_seen[y][lh], y]).unwrap()
+                    *= SIZE - 1 - sizes_last_seen[y][lh];
+            }
+        }
+    }
 }
 
 fn test_view(acc: &mut ndarray::ArrayBase<ndarray::OwnedRepr<i32>, ndarray::Dim<[usize; 2]>>, 
@@ -75,8 +96,7 @@ pub fn solve() -> SolutionPair {
     
     // Part 2
     let mut b = Array2::<usize>::ones(a.raw_dim());
-    get_los(&mut b, &mut a.axis_iter(Axis(0)).enumerate(), false);
-    get_los(&mut b, &mut a.axis_iter(Axis(0)).rev().enumerate(), true);
+    get_los(&mut b, &a);
 
     let sol2: u64 = *b.iter().max().unwrap() as u64;
 
