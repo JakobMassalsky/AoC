fdb9be66c746209bd341489f7e0841df06414d9b
jakob maßalsky <jakob.massalsky@gmail.com>
2022-12-15 20:18:56 +0100

    day 15 fast part 2 fix

--- AoC_22/AoC_22/src/days/day_15.rs
+++ AoC_22/AoC_22/src/days/day_15.rs
@@ -8,10 +8,9 @@ pub fn solve() -> SolutionPair {
 
     const Y: i64 = 2000000;
     // const Y: i64 = 10;
-    const MAX: i64 = 4000000;
 
     let lines = utils::read_lines("./input/input_15");
-    let mut sensors = lines.iter().map(|s| extract_ints(s)).collect_vec();
+    let mut sensors = lines.iter().map(|s| utils::extract_ints(s)).collect_vec();
     for sensor in sensors.iter_mut() {
         let d = get_sensor_d(&sensor);
         sensor.push(d);
@@ -50,6 +49,7 @@ pub fn solve() -> SolutionPair {
 
     /*  Walk around the edges of each diamond and find the 
         first point that is not in range of another sensor */ 
+    // const MAX: i64 = 4000000;
     // for points in sensors.iter() {
     //     let (xs, ys) = (points[0], points[1]);
     //     let md = points[4];
@@ -69,26 +69,29 @@ pub fn solve() -> SolutionPair {
         let d = (s1[0] - s2[0]).abs() + (s1[1] - s2[1]).abs();
         if d == s1[4] + s2[4] + 2 {
             pairs.push([s1, s2]);
+            if pairs.len() == 2 { 
+                break; 
+            }
         }
     }
-    let x1 = (pairs[0][0][0] + pairs[0][1][0]) / 2;
-    let y1 = (pairs[0][0][1] + pairs[0][1][1]) / 2;
-    let x2 = (pairs[1][0][0] + pairs[1][1][0]) / 2;
-    let y2 = (pairs[1][0][1] + pairs[1][1][1]) / 2;
-    if (pairs[0][0][0] - pairs[0][1][0]).signum() + (pairs[0][0][1] + pairs[0][1][1]).signum() == 0 {
-        sol2 = get_point(x1, y1, x2, y2); // up right
+    let s1 = (pairs[0][1][0] - pairs[0][0][0]).signum();
+    let s2 = (pairs[1][1][0] - pairs[1][0][0]).signum();
+    let x1 = pairs[0][0][0] + pairs[0][0][4]*s1 + s1;
+    let y1 = pairs[0][0][1];
+    let x2 = pairs[1][0][0] + pairs[1][0][4]*s2 + s2;
+    let y2 = pairs[1][0][1];
+    if s1 - (pairs[0][0][1] + pairs[0][1][1]).signum() == 0 {
+        sol2 = get_point(x2, y2, x1, y1); // up left
     } else {
-        sol2 = get_point(x2, y2, x1, y1);
+        sol2 = get_point(x1, y1, x2, y2); // up right
     }
 
     (Solution::I64(sol1), Solution::I64(sol2))
 }
 
 fn get_point(x1: i64, y1: i64, x2: i64, y2: i64) -> i64 {
-    let a = x1 / y1;
-    let b = x2 * y2;
-    let y = ((b / a) as f64).sqrt() as i64;
-    let x = a * y;
+    let x = (x1 + x2 - y1 + y2) / 2;
+    let y = (-x1 + x2 + y1 + y2) / 2;
     x * 4000000 + y
 }
 
@@ -105,21 +108,4 @@ fn is_in_sensor_range(p: [i64; 2], sensors: &Vec<Vec<i64>>, max: i64) -> i64 {
 fn get_sensor_d(points: &Vec<i64>) -> i64 {
     let (xs, ys, xb, yb) = (points[0], points[1], points[2], points[3]);
     (xs - xb).abs() + (ys - yb).abs()
-}
-
-fn extract_ints(line: &String) -> Vec<i64> {
-    let mut num_buff = vec![];
-    let mut nums = vec![];
-    for c in line.chars() {
-        if c.is_numeric() || c == '-' {
-            num_buff.push(c);
-        } else if !num_buff.is_empty() {
-            nums.push(num_buff.iter().collect::<String>().parse::<i64>().unwrap());
-            num_buff.clear();
-        }
-    }
-    if !num_buff.is_empty() {
-        nums.push(num_buff.iter().collect::<String>().parse::<i64>().unwrap());
-    }
-    nums
 }
--- AoC_22/AoC_22/src/etc/utils.rs
+++ AoC_22/AoC_22/src/etc/utils.rs
@@ -15,3 +15,20 @@ where P: AsRef<Path> {
     }
     return l;
 }
+
+pub fn extract_ints(line: &String) -> Vec<i64> {
+    let mut num_buff = vec![];
+    let mut nums = vec![];
+    for c in line.chars() {
+        if c.is_numeric() || c == '-' {
+            num_buff.push(c);
+        } else if !num_buff.is_empty() {
+            nums.push(num_buff.iter().collect::<String>().parse::<i64>().unwrap());
+            num_buff.clear();
+        }
+    }
+    if !num_buff.is_empty() {
+        nums.push(num_buff.iter().collect::<String>().parse::<i64>().unwrap());
+    }
+    nums
+}
