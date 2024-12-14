ca19d3f4304a17196c7fcc66aa3423ae14bbca6f
jakob maßalsky <jakob.massalsky@gmail.com>
2024-12-14 12:12:27 +0100

    day 13 + 14

--- AoC_24/AoC_24/src/days/day_13.rs
+++ AoC_24/AoC_24/src/days/day_13.rs
@@ -1,15 +1,56 @@
-use crate::{Solution, SolutionPair, etc::utils};
+use itertools::Itertools;
+
+use crate::{etc::utils::{self, extract_ints, SumBy}, Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
+fn get_coins((a, b, goal): (Vec<i64>, Vec<i64>, Vec<i64>)) -> u64 {
+
+    let mut price = 0;
+    let (mut curx, mut cury) = (goal[0], goal[1]);
+
+    while (curx % b[0] != 0 || cury % b[1] != 0 || (curx / b[0] != cury / b[1])) && price < 300 && curx >= a[0] && cury >= a[1] {
+        price += 3;
+        curx -= a[0];
+        cury -= a[1];
+    }
+    if curx % b[0] != 0 || cury % b[1] != 0 || curx / b[0] > 100 {
+        0
+    } else {
+        (price + curx / b[0]) as u64
+    }
+
+}
+
+fn get_coins2((a, b, goal): (Vec<i64>, Vec<i64>, Vec<i64>)) -> u64 {
+
+    let (ax, ay) = (a[0], a[1]);
+    let (bx, by) = (b[0], b[1]);
+    let (x, y) = (goal[0] + 10000000000000, goal[1] + 10000000000000);
+    
+    let m = (ay*x - ax*y)/(ay*bx - ax*by);
+    let n = (x - bx * m) / ax;
+    
+    if ax * n + bx * m == x && ay * n + by * m == y {
+        (m + 3 * n) as u64
+    } else {
+        0
+    }
+}
 
 pub fn solve() -> SolutionPair {
 
-    // let lines = utils::read_lines("./input/input_01");
+    let lines: Vec<Vec<i64>> = utils::read_lines("./input/input_13").into_iter()
+        .map(|line| extract_ints(&line, &[]))
+        .enumerate()
+        .filter_map(|(i, e)| if i % 4 != 3 { Some(e) } else { None })
+        .collect_vec();
+
+    // println!("{:?}", lines);
 
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let sol1: u64 = lines.clone().into_iter().tuples().sum_by(get_coins);
+    let sol2: u64 = lines.into_iter().tuples().sum_by(get_coins2);
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
--- AoC_24/AoC_24/src/days/day_14.rs
+++ AoC_24/AoC_24/src/days/day_14.rs
@@ -1,15 +1,52 @@
-use crate::{Solution, SolutionPair, etc::utils};
+use std::io::{self, Write};
+
+use itertools::Itertools;
+
+use crate::{etc::utils::{self, extract_ints}, Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
+const W: i64 = 101;
+const H: i64 = 103;
+
+fn move_robot(parms: &Vec<i64>, i: i64) -> (i64, i64) {
+    ((parms[0] + parms[2] * i).rem_euclid(W), (parms[1] + parms[3] * i).rem_euclid(H))
+}
 
 pub fn solve() -> SolutionPair {
 
-    // let lines = utils::read_lines("./input/input_01");
+    let lines = utils::read_lines("./input/input_14").into_iter()
+        .map(|line| extract_ints(&line, &['-'])).collect_vec();
+
+    let mut p = 0;
+
+    for i in 0..10000 {
+        let pos = lines.iter().map(|p| move_robot(p, i)).collect_vec();
+        if pos.iter().map(|(x, y)| x * W + y).unique().sorted().tuple_windows()
+            .any(|(i1, i2, i3, i4, i5, i6, i7, i8) | 
+                i1 + 1 == i2 && i2 + 1 == i3 && i3 + 1 == i4 && i4 + 1 == i5 && i5 + 1 == i6 && i6 + 1 == i7 && i7 + 1 == i8) {
+            p = i;
+            let mut img = vec![vec![' '; W as usize]; H as usize];
+            for (x, y) in pos {
+                img[y as usize][x as usize] = '#'
+            }
+            for row in img {
+                println!("{}", row.into_iter().join(""));
+            }
+            break;
+        }
+    }
+
+    let pos = lines.iter().map(|p| move_robot(p, 100)).collect_vec();
+
+    let q1 = pos.iter().filter(|(x, y)| *x < W / 2 && *y < H / 2).count();
+    let q2 = pos.iter().filter(|(x, y)| *x > W / 2 && *y < H / 2).count();
+    let q3 = pos.iter().filter(|(x, y)| *x < W / 2 && *y > H / 2).count();
+    let q4 = pos.iter().filter(|(x, y)| *x > W / 2 && *y > H / 2).count();
 
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let sol1: u64 = (q1 * q2 * q3 * q4) as u64;
+    let sol2: u64 = p as u64;
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
